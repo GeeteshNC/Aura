0ea33abea78db1cf3a5481d22ebf15ec8b92ce13
Nallan Chakravarthula Geetesh <geeteshacharya@gmail.com>
2025-09-15 12:52:10 +0530

    Effect actor Blue prints implementation

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -23,13 +23,9 @@ protected:
 	virtual void BeginPlay() override;
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
-	UPROPERTY(EditAnywhere, Category="Applied Effects")
+	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category="Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 	
-	
-
-
-
 
 
 };
