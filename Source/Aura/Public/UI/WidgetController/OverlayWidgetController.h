44fecaa301a0a355ddc0503b7b992ca0ec06bf71
Nallan Chakravarthula Geetesh <geeteshacharya@gmail.com>
2025-09-24 16:45:21 +0530

    GhostGlobe

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -1,4 +1,4 @@
-// Copy right GeetesNC
+// Copy right GeetesNC .h
 
 #pragma once
 
@@ -29,10 +29,10 @@ struct FUIWidgetRow: public FTableRowBase
 	
 };
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+class UAuraWidget;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeChangedSignature, float, NewValue);
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 
 
@@ -50,16 +50,16 @@ public:
 	virtual void BindCallbacksToDependencies() override;
 	
     UPROPERTY(BlueprintAssignable, Category = "GAS/Attributes")	
-	FOnHealthChangedSignature OnHealthChanged;
+	FOnAttributeChangedSignature OnHealthChanged;
 	
 	UPROPERTY(BlueprintAssignable, Category = "GAS/Attributes")
-	FOnMaxHealthChangedSignature OnMaxHealthChanged;
+	FOnAttributeChangedSignature OnMaxHealthChanged;
 
 	UPROPERTY(BlueprintAssignable, Category = "GAS/Attributes")
-	FOnManaChangedSignature OnManaChanged;
+	FOnAttributeChangedSignature OnManaChanged;
 	
 	UPROPERTY(BlueprintAssignable, Category = "GAS/Attributes")
-	FOnMaxManaChangedSignature OnMaxManaChanged;
+	FOnAttributeChangedSignature OnMaxManaChanged;
 	
 	UPROPERTY(BlueprintAssignable, Category = "GAS/Messages")
 	FMessageWidgetRowSignature MessageWidgetRowDelegate;
@@ -68,10 +68,7 @@ protected:
 UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,Category = "WidgetData")
 	TObjectPtr<UDataTable> MessageWidgetDataTable;
 	
-	void HealthChanged(const FOnAttributeChangeData& Data) const;
-	void MaxHealthChanged(const FOnAttributeChangeData& Data)const;
-	void ManaChanged(const FOnAttributeChangeData& Data) const;
-	void MaxManaChanged(const FOnAttributeChangeData& Data) const;
+
 
 	template<typename T>
 	T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
@@ -95,4 +92,5 @@ T* UOverlayWidgetController::GetDataTableRowByTag(UDataTable* DataTable, const F
 	
 	T* (Row)=DataTable->FindRow<T>((Tag).GetTagName(), TEXT(""));
 	return Row; */
+
 }
