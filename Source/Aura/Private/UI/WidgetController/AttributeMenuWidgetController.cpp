2ed04b40582712576f587e8a40d7a4cb4e90751e
Nallan Chakravarthula Geetesh <geeteshacharya@gmail.com>
2025-10-05 19:01:23 +0530

    Aura Blueprint function Library

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copy right GeetesNC
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
