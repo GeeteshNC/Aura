425133ef6d1978e3ccd6b6c36d55f245e8d726ec
Nallan Chakravarthula Geetesh <geeteshacharya@gmail.com>
2025-10-06 19:15:22 +0530

    Constructing the Attribute Menu Widget Controller

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -2,9 +2,8 @@
 
 
 #include "AbilitySystem/AuraAbilitySystemLibrary.h"
-
 #include "Kismet/GameplayStatics.h"
-#include "UI/WidgetController/OverlayWidgetController.h"
+#include "UI/WidgetController/AuraWidgetController.h"
 #include "Player/AuraPlayerState.h"
 #include "UI/HUD/AuraHUD.h"
 
@@ -36,4 +35,31 @@ return nullptr;
 	
 	
 	
+}
+
+UAttributeMenuWidgetController* UAuraAbilitySystemLibrary::GetAttributeMenuWidgetController(
+	const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject,0))
+		
+	{
+
+		if (AAuraHUD* AuraHUD= Cast<AAuraHUD>( PC->GetHUD()))
+
+
+		{
+
+
+			AAuraPlayerState* PS =PC->GetPlayerState<AAuraPlayerState>();
+
+			UAbilitySystemComponent* ASC= PS->GetAbilitySystemComponent();
+			UAttributeSet* AS= PS->GetAttributeSet();
+
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return AuraHUD->GetAttributeMenuWidgetController(WidgetControllerParams);
+			
+			
+		}
+	}
+	return nullptr;
 }
--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -10,6 +10,12 @@
 class UOverlayWidgetController;
 class UAuraUserWidget;
 struct FWidgetControllerParams;
+class UAttributeMenuWidgetController;
+class UAbilitySystemComponent;
+class UAttributeSet;
+
+
+
 
 /**
  * 
@@ -19,10 +25,11 @@ class AURA_API AAuraHUD : public AHUD
 {
 	GENERATED_BODY()
 public:
-	UPROPERTY()
-	TObjectPtr<UAuraUserWidget>OverlayWidget;
+
 
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+	UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams);
+	
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 
@@ -31,12 +38,22 @@ protected:
 	
 	
 private:
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget>OverlayWidget;
+	
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UAuraUserWidget>OverlayWidgetClass;
 	UPROPERTY()
 	TObjectPtr<UOverlayWidgetController> OverlayWidgetController;
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
+    UPROPERTY()
+	TObjectPtr<UAttributeMenuWidgetController> AttributeMenuWidgetController;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAttributeMenuWidgetController> AttributeMenuWidgetControllerClass;
+	
+
 	
 	
 };
