c587e39655c5959bc5a4d2ab6c4c5a0addb299a1
Nallan Chakravarthula Geetesh <geeteshacharya@gmail.com>
2025-10-25 20:25:00 +0530

    AbilitySystemGlobals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,10 @@
+// Copy right GeetesNC
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
